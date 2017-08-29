f2a0b36290caa1fa1ac53bfc294ec1651ad3b5a2
homs <homspage@gmail.com>
2017-08-29 18:04:47 +0300

    amedia added

--- js/main.js
+++ js/main.js
@@ -5,14 +5,10 @@ let  bottom = document.querySelector(`.bottom-back`);
 document.addEventListener(`scroll`, show_bottom);
 
 function show_bottom () {
-  console.log(window.innerHeight);
   if (window.scrollY > window.innerHeight) {
-    console.log('if');
-
     bottom.style.zIndex = '2';
   }
   else {
-    console.log('else');
     bottom.style.zIndex = '0';
   }
 }
