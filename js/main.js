c4f2bb62d56f66f271a852a88f952cc44ecd54ea
homs <homspage@gmail.com>
2017-08-31 14:37:57 +0300

    Margins to front added, loader added, some formating

--- js/main.js
+++ js/main.js
@@ -4,6 +4,17 @@ let  bottom = document.querySelector(`.bottom-back`);
 
 document.addEventListener(`scroll`, show_bottom);
 
+
+
+
+window.onload = disableLoader(`.loader`);
+
+function disableLoader(loaderClass) {
+  setTimeout(() => {
+    document.querySelector(loaderClass).style.display = `none`;
+  }, 100)
+}
+
 function show_bottom () {
   if (window.scrollY > window.innerHeight) {
     bottom.style.zIndex = '2';
