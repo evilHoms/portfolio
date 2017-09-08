34a688938c7751c525a88c01ea4ea8e6f77ebac2
homs <homspage@gmail.com>
2017-09-08 13:15:20 +0300

    Added slider between pages, refactoring front and pages code

--- js/main.js
+++ js/main.js
@@ -9,11 +9,14 @@ const about = document.querySelector(`.about`);
 const works = document.querySelector(`.works`);
 const contacts = document.querySelector(`.contacts`);
 
+const frontWrapper = document.querySelector(`.front`);
 const aboutPage = document.querySelector(`.about-page`);
 const worksPage = document.querySelector(`.works-page`);
 const contactsPage = document.querySelector(`.contacts-page`);
 
 let currentPage = `about`;
+
+initStartPage();
 
 about.addEventListener(`click`, aboutClicked);
 works.addEventListener(`click`, worksClicked);
@@ -21,7 +24,6 @@ contacts.addEventListener(`click`, contactsClicked);
 
 window.addEventListener(`load`, disableLoader);
 document.addEventListener(`scroll`, documentScrolled());
-
 
 function disableLoader() {
   setTimeout(() => {
@@ -48,7 +50,7 @@ function documentScrolled () {
 
   function isScrollOnTop() {
     let screenHeight = window.innerHeight;
-    console.log(screenHeight);
+
     if (window.scrollY < screenHeight - sneakyPanel.offsetHeight) {
       return true;
     }
@@ -86,9 +88,11 @@ function documentScrolled () {
 function aboutClicked() {
   if (currentPage != `about`) {
     currentPage = `about`;
+    changeHeight(frontWrapper, aboutPage);
+    slidePosition();
     console.log(`About clicked`);
 
-    aboutPage.style.position = `relative`;
+    /*aboutPage.style.position = `relative`;
     aboutPage.style.zIndex = `3`;
     worksPage.style.position = `absolute`;
     worksPage.style.zIndex = `0`;
@@ -96,15 +100,18 @@ function aboutClicked() {
     contactsPage.style.zIndex = `0`;
     contactsPage.style.display = `none`;
     aboutPage.style.display = `block`;
-    worksPage.style.display = `none`;
+    worksPage.style.display = `none`;*/
   }
 }
 
 function worksClicked() {
   if (currentPage != `works`) {
     currentPage = `works`;
+    changeHeight(frontWrapper, worksPage);
+    slidePosition();
     console.log(`Works clicked`);
-    worksPage.style.position = `relative`;
+
+    /*worksPage.style.position = `relative`;
     worksPage.style.zIndex = `3`;
     aboutPage.style.position = `absolute`;
     aboutPage.style.zIndex = `0`;
@@ -112,15 +119,18 @@ function worksClicked() {
     contactsPage.style.zIndex = `0`;
     contactsPage.style.display = `none`;
     aboutPage.style.display = `none`;
-    worksPage.style.display = `block`;
+    worksPage.style.display = `block`;*/
   }
 }
 
 function contactsClicked() {
   if (currentPage != `contacts`) {
     currentPage = `contacts`;
+    changeHeight(frontWrapper, contactsPage);
+    slidePosition();
     console.log(`Contacts clicked`);
-    contactsPage.style.position = `relative`;
+
+    /*contactsPage.style.position = `relative`;
     contactsPage.style.zIndex = `3`;
     worksPage.style.position = `absolute`;
     worksPage.style.zIndex = `0`;
@@ -128,7 +138,39 @@ function contactsClicked() {
     aboutPage.style.zIndex = `0`;
     contactsPage.style.display = `block`;
     aboutPage.style.display = `none`;
-    worksPage.style.display = `none`;
+    worksPage.style.display = `none`;*/
   }
 }
 
+function changeHeight(pageToBeChanged, pageChangeFrom) {
+  pageToBeChanged.style.height = `${pageChangeFrom.offsetHeight}px`;
+}
+
+function slidePosition(curPage = currentPage, wrapper = frontWrapper) {
+  switch (curPage) {
+    case `about`:
+      aboutPage.style.left = 0;
+      worksPage.style.left = `${-wrapper.offsetWidth}px`;
+      contactsPage.style.left = `${wrapper.offsetWidth}px`;
+      break;
+    case `works`:
+      worksPage.style.left = 0;
+      aboutPage.style.left = `${wrapper.offsetWidth}px`;
+      contactsPage.style.left = `${wrapper.offsetWidth * 2}px`;
+      break;
+    case `contacts`:
+      contactsPage.style.left = 0;
+      worksPage.style.left = `${-wrapper.offsetWidth * 2}px`;
+      aboutPage.style.left = `${-wrapper.offsetWidth}px`;
+      break;
+    default:
+      console.log(`Some problem in slidePosition function`);
+  }
+}
+
+function initStartPage(arrOfPages = [aboutPage, worksPage, contactsPage], wrapper = frontWrapper) {
+  wrapper.style.height = `${arrOfPages[0].offsetHeight}px`;
+  arrOfPages[0].style.left = 0;
+  arrOfPages[1].style.left = `${-wrapper.offsetWidth}px`;
+  arrOfPages[2].style.left = `${wrapper.offsetWidth}px`; 
+}
